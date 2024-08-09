3426b974eb772810c473c63dffb9e79812e1dbb2
Brian Kipkirui <bryokn@gmail.com>
2024-08-09 14:07:31 +0300

    FONT CHANGES

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Skills from './components/Skills';
 import Background from './components/Background';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import './styles/fonts.css';
 import './App.css';
 
 function App() {
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -13,7 +13,7 @@ const skillsSection = [
   {name: "Database", class: "database", animation: "database.json", 
     skills: [{name: "PostgreSQL", icon: "postgre-sql.svg"}, {name: "MySQL", icon: "mysql.svg"}, {name: "MongoDB", icon: "mongodb.svg"}, {name: "MS Access", icon: "ms-access.svg"}] },
   {name: "Other Technologies", class: "other-tech", animation: "other-tech.json", 
-    skills: [{name: "Git", icon: "git.svg"}, {name: "GitHub", icon: "github.svg"}, {name: "MS Excel", icon: "ms-excel.svg"}] }
+    skills: [{name: "Git", icon: "git.svg"}, {name: "GitHub", icon: "github.svg"}, {name: "Expo", icon: "expo.svg"}, {name: "Postman", icon: "postman.svg"}, {name: "SpringBoot", icon: "spring.svg"}] }
   ];
 
   
